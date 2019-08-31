90868522cde3e93795cc945689be8259896c1390
justynamak <justyna.zdunkowska@gmail.com>
2019-08-31 13:45:50 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/timer/precache-manifest.9020064798b34951cd437b49ec6f2c56.js"
+  "/timer/precache-manifest.2c8abe9911e18d001b7187f83bbc5e4b.js"
 );
 
 self.addEventListener('message', (event) => {

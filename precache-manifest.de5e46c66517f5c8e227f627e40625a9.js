0ade85650e6ac8c9e9fbce349d60ded2fc23708b
justynaMak <justynamakuch@outlook.com>
2019-11-25 10:56:06 +0100

    Updates

---
+++ precache-manifest.de5e46c66517f5c8e227f627e40625a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c2bce5f83ab54afcacb875aa2c8e9f",
+    "url": "/timer-create-react-app/index.html"
+  },
+  {
+    "revision": "4b125731ae7539f5f9d4",
+    "url": "/timer-create-react-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "beb0c3464497171f5843",
+    "url": "/timer-create-react-app/static/css/main.ba393643.chunk.css"
+  },
+  {
+    "revision": "4b125731ae7539f5f9d4",
+    "url": "/timer-create-react-app/static/js/2.dd8038f1.chunk.js"
+  },
+  {
+    "revision": "beb0c3464497171f5843",
+    "url": "/timer-create-react-app/static/js/main.a6de000b.chunk.js"
+  },
+  {
+    "revision": "14c3a2d5e3706ae07d8b",
+    "url": "/timer-create-react-app/static/js/runtime~main.828ee6f5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/timer/precache-manifest.2c8abe9911e18d001b7187f83bbc5e4b.js"
+  "/timer-create-react-app/precache-manifest.de5e46c66517f5c8e227f627e40625a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/timer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/timer-create-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
